0416c44cf33b4086dd1953b9d73e0ddca8aee730
Александр Пичугин <aleksandrpicugin700@gmail.com>
2025-03-19 23:03:12 +0300

    sSchedule in progress

---
+++ DML/SQLQuery0- AGGREGATION.sql
@@ -0,0 +1,16 @@
+--USE PV_319_DDL;
+--GO
+
+--SELECT
+--				group_name			AS N'Группа',
+--				COUNT(student_id)	AS N'Количество студентов'
+--FROM			Students, Groups
+--WHERE			[group]=group_id
+--GROUP BY		group_name
+--ORDER BY		N'Количество студентов'
+--;
+
+USE PV_319_Import;
+GO
+
+SELECT * FROM Students;
